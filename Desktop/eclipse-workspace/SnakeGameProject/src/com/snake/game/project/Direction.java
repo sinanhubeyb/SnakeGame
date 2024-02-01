6ae5ac2fef5d3fb4e3e0d91998677294171e0cda
Sinan <snnhbyb@gmail.com>
2024-02-01 13:18:11 +0100

    initial commit

---
+++ Desktop/eclipse-workspace/SnakeGameProject/src/com/snake/game/project/Direction.java
@@ -0,0 +1,25 @@
+package com.snake.game.project;
+
+public enum Direction {
+	
+	/**
+	 * Moving North (Up).
+	 */
+	North,
+	
+	/**
+	 * Moving East (Right).
+	 */
+	East,
+	
+	/**
+	 * Moving South (Down).
+	 */
+	South,
+	
+	/**
+	 * Moving West (Left).
+	 */
+	West
+
+}
---
+++ Desktop/eclipse-workspace/SnakeGameProject/src/com/snake/game/project/TileType.java
@@ -0,0 +1,11 @@
+package com.snake.game.project;
+
+public enum TileType {
+	
+Fruit,
+	
+	SnakeHead,
+	
+	SnakeBody
+
+}
